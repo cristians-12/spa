2a66f7ec77ef50fa3a2bbcb9d87749832c3733e4
Abrahan-riwi26 <abrahan.villa@riwi.io>
2026-05-26 18:30:20 -0500

    add readme

--- src/main.js
+++ src/main.js
@@ -4,13 +4,3 @@ import { router } from "./router/router";
 window.addEventListener("DOMContentLoaded", router);
 
 window.addEventListener("popstate", router);
-
-window.addEventListener('popstate', (event) => {
-  console.log("El usuario ha cambiado de página en el historial");
-  
-  if (event.state) {
-    console.log("Estado restaurado:", event.state);
-  }
-});
-
-history.pushState({}, "", '/')
